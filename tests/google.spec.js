5e5d05ae703489b1d0d5d989a7acffaf8c459d5c
reshma786-debug <reshmarattu786@gmail.com>
2026-07-12 20:01:11 +0530

    Latest changes

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -3,12 +3,8 @@ const {test,expect} = require('@playwright/test')
 test("Verify Application Title", async ({page})=>{
 
     await page.goto("https://www.google.com")
-
     const url = await page.url();
-
     const title = await page.title();
-
     console.log("Title is "+ title)
-
     await expect(page).toHaveTitle("Google")
 })
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,19 +3,12 @@ const {test,expect} = require('@playwright/test')
 test("Valid login", async ({page})=>{
 
     await page.goto("https://opensource-demo.orangehrmlive.com")
-
     await page.getByPlaceholder("Username").type("Admin")
-
     await page.locator("input[name=password]").type("admin123")
-
-    await page.locator("//button[@type='submit']").click();
-
-    await expect(page).toHaveURL(/dashboard/);
-
-    await page.getByAltText("profile picture").click();
-
-    await page.getByText("Logout").click();
-
-    await expect(page).toHaveURL(/login/);
+    await page.locator("//button[@type='submit']").click()
+    await expect(page).toHaveURL(/dashboard/)
+    await page.getByAltText("profile picture").click()
+    await page.getByText("Logout").click()
+    await expect(page).toHaveURL(/login/)
     
 })
