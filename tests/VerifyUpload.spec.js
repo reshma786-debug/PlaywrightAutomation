274f98b26b523ce5136d2ee953a89a45a339b221
reshma786-debug <reshmarattu786@gmail.com>
2026-07-12 22:34:20 +0530

    changes in File Upload Class

--- tests/VerifyUpload.spec.js
+++ tests/VerifyUpload.spec.js
@@ -3,7 +3,9 @@ const {test,expect} = require ('@playwright/test')
 test("Verify Upload", async ({page}) => {
 
     await page.goto("https://the-internet.herokuapp.com/upload")
-    await page.getByLabel("#file-upload").setInputFiles("C:\Users\Reshma\Downloads\Teddy")
-    await page.getByLabel("#Upload").click()
-
+    await page.locator("#file-upload").setInputFiles("/Users/Reshma/Downloads/Teddy.avif")
+    await page.locator("#file-submit").click()
+    const fileSuccessMsg = await page.getByText("File Uploaded!").textContent()
+      console.log("fileSuccessMsg :"+ fileSuccessMsg);
+    expect(fileSuccessMsg.includes("File Uploaded!")).toBeTruthy()
 })
