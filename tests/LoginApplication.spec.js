04221a48213f9d230d984e86e8b9e2284b5ba752
reshma786-debug <reshmarattu786@gmail.com>
2026-07-14 10:42:51 +0530

    POM - Home Page added

--- tests/LoginApplication.spec.js
+++ tests/LoginApplication.spec.js
@@ -1,9 +1,15 @@
+
 const {test,expect} = require('@playwright/test'); 
-const LoginPage = require("../pages/loginpage")
+const LoginPage = require('./Pages/LoginPage');
+const HomePage = require('./Pages/HomePage');
 
 test('Login To Application using POM', async ({ page }) =>
 {
     await page.goto('https://freelance-learn-automation.vercel.app/login')
     const loginPage = new LoginPage(page)
     await loginPage.loginToApplication()
+
+    const homepage = new HomePage(page)
+    await homepage.verifyManageOption()
+    await homepage.logoutApplication()
 });
--- tests/pages/loginpage.js
+++ tests/pages/loginpage.js
@@ -1,18 +1,16 @@
-class LoginPage{
+class LoginPage {
 
-constructor(page)
-{
-    this.page =page
-    this.userName="#email1"
-    this.password="//input[@placeholder='Enter Password']"
-    this.loginbutton="//button[text()='Sign in']"
-}
-    async loginToApplication()
-    {
-        await this.page.fill(this.userName,"admin@gmail.com")
-        await this.page.fill(this.password,"admin@123")
+    constructor(page) {
+        this.page = page
+        this.userName = "#email1"
+        this.password = "//input[@placeholder='Enter Password']"
+        this.loginbutton = "//button[text()='Sign in']"
+    }
+    async loginToApplication() {
+        await this.page.fill(this.userName, "admin@email.com")
+        await this.page.fill(this.password, "admin@123")
         await this.page.click(this.loginbutton)
     }
 }
 
-module.exports=LoginPage;
+module.exports = LoginPage;
