ee9bff6e85602aa788e013c387153e43b7a6a75f
reshma786-debug <reshmarattu786@gmail.com>
2026-07-23 18:40:07 +0530

    Handle Auto Complete code - Changes

--- tests/HandleAutoComplete.spec.js
+++ tests/HandleAutoComplete.spec.js
@@ -3,5 +3,15 @@
 
  test ("Handle Auto Complete",  async ({page}) =>
  {
+      await page.goto("https://www.google.com")
 
+     await page.locator("//textarea[@name='q']").fill("playwright")
+
+     await page.waitForSelector("//li[@role='presentation']")
+
+      const element = await page.$$("//li[@role='presentation']")
+
+     await page.keyword.press("ArrowDown")
+
+     await page.keyboard.press("Enter")
  })
