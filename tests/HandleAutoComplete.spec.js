24445e22e8950f22f1aa6da9f47807e7356a0fdc
reshma786-debug <reshmarattu786@gmail.com>
2026-07-27 12:13:36 +0530

    Format changes in code

--- tests/HandleAutoComplete.spec.js
+++ tests/HandleAutoComplete.spec.js
@@ -1,17 +1,10 @@
 
- const{test,expect}=require ('@playwright/test')
-
- test ("Handle Auto Complete",  async ({page}) =>
- {
-      await page.goto("https://www.google.com")
-
-     await page.locator("//textarea[@name='q']").fill("playwright")
-
-     await page.waitForSelector("//li[@role='presentation']")
-
-      const element = await page.$$("//li[@role='presentation']")
-
-     await page.keyword.press("ArrowDown")
-
-     await page.keyboard.press("Enter")
- })
+const { test, expect } = require('@playwright/test')
+test("Handle Auto Complete", async ({ page }) => {
+    await page.goto("https://www.google.com")
+    await page.locator("//textarea[@name='q']").fill("playwright")
+    await page.waitForSelector("//li[@role='presentation']")
+    const element = await page.$$("//li[@role='presentation']")
+    await page.keyword.press("ArrowDown")
+    await page.keyboard.press("Enter")
+})
