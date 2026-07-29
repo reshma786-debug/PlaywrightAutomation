a8679480d9db3f2135b68d4b8829ca949e0aef9c
reshma786-debug <reshmarattu786@gmail.com>
2026-07-29 18:56:24 +0530

    changes in handleautocomplete.spec.js

--- tests/HandleAutoComplete.spec.js
+++ tests/HandleAutoComplete.spec.js
@@ -3,13 +3,14 @@ const { test, expect } = require('@playwright/test')
 
 test('Handle Auto Complete', async ({ page }) => {
     await page.goto('https://www.google.com')
-
     const searchInput = page.locator("//textarea[@name='q']")
+    await searchInput.click()
     await searchInput.fill('playwright')
+    await page.waitForTimeout(2000) 
     const suggestionItem = page.locator("//li[@role='presentation' and @data-attrid='AutocompletePrediction']").first()
-    await expect(suggestionItem).toBeVisible({ timeout: 100000 })   
+    await expect(suggestionItem).toBeVisible({ timeout: 100000 })
     await page.keyboard.press('ArrowDown')
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(3000) 
     await page.keyboard.press('Enter')
-    await expect(page.locator('input[name="q"]')).toBeVisible()
+    //await expect(page.locator('input[name="q"]')).toHaveValue(/playwright/i)
 })
